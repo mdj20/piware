f152e29b3d2545b95ccb39c6e47ed7eddf8ad2e0
Matthew Jeffreys <mdj20@pitt.edu>
2016-02-09 21:08:45 -0500

    buffer and worker threads

--- multi_piware.c
+++ multi_piware.c
@@ -6,19 +6,30 @@
 
 // globals 
 int SIZE_OF_BUFFER = 1024;
-int NUM_THREADS = 3;
+int NUM_WORKERS = 0;
 int PTHREAD_STACK_SIZE = 0;  // not used
 
 // thread function declaration
 void *down_work(void *);
+void *buffer_filler(void* args);
 
+
+
+// header for any structure in the buffer
+typedef struct _buffer_element_header {
+
+  char instruction;  // indicates what kind of message buffer item is
+  int size;  // indicates size of buffer message
+
+} buffer_element_header;
 
 // struct for buffer info
-typedef struct _buffer_data {
-  void* buffer_addr;
-  int   buffer_size;
-  
-} buffer_data;
+typedef struct _buffer_thread_data {
+  void *buffer_addr;
+  int buffer_size;
+  pthread_mutex_t buf_mutex;
+  char run_flag;
+} buffer_thread_data;
  
 // worker arguments 
 typedef struct _worker_thread_data {
@@ -38,32 +49,37 @@ int main(int argc , char* argv[]){
   memset(uplink_buffer,0,SIZE_OF_BUFFER);
   memset(downlink_buffer,0,SIZE_OF_BUFFER);
 
-  pthread_t threads[NUM_THREADS];
-  worker_thread_data *d_structs[NUM_THREADS];
+  pthread_t buffer_threads[2];
+  buffer_thread_data *b_structs[2];
+
+  // launch buffer thread
+  // create struct
+  b_structs[0] = malloc(sizeof(buffer_thread_data));
+  b_structs[0]->buffer_addr = uplink_buffer;
+  b_structs[0]->buffer_size = 0;
+  pthread_mutex_init(&(b_structs[0]->buf_mutex),NULL);
+  b_structs[0]->run_flag = 1;
+
+  pthread_create(&buffer_threads[i],NULL,buffer_filler,b_structs[0]);
+
+  pthread_t worker_threads[NUM_WORKERS];
+  worker_thread_data *d_structs[NUM_WORKERS];
 
   //pthread_t *test_t = 
-  for(i=0;i<NUM_THREADS;i++){
+  for(i=0;i<NUM_WORKERS;i++){
     d_structs[i] = malloc(sizeof(worker_thread_data));		      
     d_structs[i]->n = i;				      
-    pthread_create(&threads[i],NULL,down_work,d_structs[i]);
+    pthread_create(&worker_threads[i],NULL,down_work,d_structs[i]);
   }
-
-  for(i=0;i<1000000;i++){
-    j++;
-  }
-
-  printf("this is the number: %d\n",j);
 
   sleep(2);
+ 
+  b_structs[0]->run_flag = 0;
+  pthread_join(buffer_threads[0], NULL);
+  printf("retval: %d\n",b_structs[0]->buffer_size);
 
-  void **ret = malloc(sizeof(int));
-
-  for(i=0;i<NUM_THREADS;i++){
-    //    printf("thread num: %d\n",threads[i]);
-  }
-
-  for(i=0;i<NUM_THREADS;i++){
-    pthread_join(threads[i],ret);
+  for(i=0;i<NUM_WORKERS;i++){
+    pthread_join(worker_threads[i],NULL);
   }
 }
 
@@ -85,11 +101,25 @@ void *down_work(void *args){
 // this thread will fill the buffers so simulte a stream of data / packets etc
 void *buffer_filler(void* args){
 
+  // parse args
+  buffer_thread_data *bufdat = (buffer_thread_data*)args;
+  
 
+  int n = 0; // number of RUNS
 
+  // run
+  while(bufdat->run_flag){
+    printf("BUFFER THREAD RUNNING....\n");
+    n++;
+    //    sleep(1);
+  }
 
+  printf("inside: %d\n",n);
 
-
+  // set size as return value
+  bufdat->buffer_size = n;
+  // exit thread no return value in pthread
+  pthread_exit(NULL);
 
 }
 
