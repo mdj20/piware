bef1e397d7add093177f5cc286c82afdb28a4619
Matthew <mdj20@pitt.edu>
2016-02-25 03:49:25 -0500

    mt_logger

---
+++ related_code/mt_log.h
@@ -0,0 +1,23 @@
+#ifndef MT_LOG_H
+#define MT_LOG_H
+
+
+
+// init recorder;
+int mt_recorder_init(size_t width, int capacity, int max);
+static int init_table(int index, size_t width, int capacity, int max_capacity);
+void free_recorder();
+int n(int index);
+int mt_add_record(int index,int* vals);
+void* records(int index );
+int check_capacity(int index);
+int multiply_capacity(int index, int x);
+int clear_records(int index);
+
+
+
+
+
+
+
+#endif
